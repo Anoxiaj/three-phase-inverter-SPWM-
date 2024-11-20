6f03d19cf6b169833e0ee80d14917a8e50fd0276
Anoxiaj <1169190960@qq.com>
2024-11-20 09:34:46 +0800

    off-gird inverter

--- parameter.h
+++ parameter.h
@@ -5,12 +5,22 @@
 
 #define switch_loop 0 // 0：双闭环  1：单闭环
 
-// 单闭环时Kp = 0.05 Ki = 5
-// 双闭环时：100: 2 300 ; 150:0.1 50
 #define U_Kp_parameter 2
 #define U_Ki_parameter 300
 
 #define I_Kp_parameter 0.05
 #define I_Ki_parameter 5
+
+/******************离网直流侧350V时*******************/
+// f = 10e3;
+// Ts = 1 / f;
+// Vdc = 350;
+// L = 2e-3;
+// C = 40e-6;
+// R = 1;
+// 电流环参数：Kp = 0.05 Ki = 5
+// 电压环参数如下：
+// 单闭环时：Kp = 0.05 Ki = 5
+// 双闭环时：100: 2 300 ; 150:0.1 50
 
 #endif
